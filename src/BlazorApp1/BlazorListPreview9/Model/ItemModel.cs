5e20ef63e65db7c0c26b2f93c0a4ae48f375310a
Linda Lawton <laurly71@gmail.com>
2019-10-07 16:41:51 +0200

    init

---
+++ src/BlazorApp1/BlazorListPreview9/Model/ItemModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorListPreview9.Model
+{
+    public class ItemModel
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+
+        [Range(13, 105, ErrorMessage = "Age invalid (13-105).")]
+        public int Age { get; set; }
+
+        [Required]
+        [Range(typeof(bool), "true", "true",
+            ErrorMessage = "This Form requiers your consent.")]
+        public bool HasConsented { get; set; }
+
+    }
+}
+
+
+
+
+
+
+
+
